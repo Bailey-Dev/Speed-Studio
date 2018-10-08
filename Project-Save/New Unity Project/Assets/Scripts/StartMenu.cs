f619ce327bf866b1e066e5c5f7a75fd2ae21cff7
Bailey <Baileykok@gmail.com>
2018-10-08 16:39:56 +0200

    Adding files

---
+++ Project-Save/New Unity Project/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+
+	public void Facebook(){
+		Application.OpenURL ("https://www.facebook.com/");
+
+	}
+	public void Discord(){
+		Application.OpenURL ("https://discord.gg/qutz7nj");
+
+	}
+	public void Quit(){
+
+		Application.Quit ();
+
+	}
+    public void startgame()
+    {
+     LoadScene.Scene
+    }
+
+}
